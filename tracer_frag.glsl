30334e25e0569cc56b9d09a8f99a91c85e4c1722
nemo1999 <tiny.nemo.fish@gmail.com>
2020-12-23 01:31:24 +0800

    repeated torus demo

--- tracer_frag.glsl
+++ tracer_frag.glsl
@@ -18,18 +18,6 @@ uniform float val5;
 uniform vec3 lightPos;
 uniform float lightSize;
 uniform float timeSinceStart;
-// record the position of the spheres
-// value of infinity  means the sphere should not be shown
-uniform vec4 sphereCenterRadius[numSphere];
-
-// record the material of the sphere
-// -inf for diffuse material
-// -k for glass with refraction constant k (k>0)
-//  0 for mirror material  
-//  g for glossiness of mirror material (g = random noise magnitude added to the reflected ray)
-uniform float sphereMaterial[numSphere];
-//record the color of each sphere
-uniform vec3 sphereColor[numSphere];
 const float pi = 3.1415926535897932384626433836795028841971;
 const float inf = 1000000000000000.0;
 const float epsilon = 0.00001; 
@@ -53,6 +41,23 @@ float Hash3d(vec3 uv)
     return fract(sin(f)*110003.9);
 }
 
+mat4 rotationMatrix(vec3 axis, float angle) {
+    axis = normalize(axis);
+    float s = sin(angle);
+    float c = cos(angle);
+    float oc = 1.0 - c;
+    
+    return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+                0.0,                                0.0,                                0.0,                                1.0);
+}
+
+vec3 rotate(vec3 v, vec3 axis, float angle) {
+	mat4 m = rotationMatrix(axis, angle);
+	return (m * vec4(v, 1.0)).xyz;
+}
+
 float random(vec3 scale, float seed){return fract(sin(dot(gl_FragCoord.xyz + seed, scale)) * 4347438.38745 + seed);}
 
 //return random point on the surface of a unit sphere
@@ -70,8 +75,24 @@ vec3 cosineWeightedDirection(float seed, vec3 normal){
   return normalize(normal + randomUnitDirection(seed));
 }
 
+float SDSphere(vec3 pos, float radius){
+  return(length(pos - vec3(0.0,0.0,0.0))-radius);
+}
+
+float SDRectangle(vec3 pos, vec3 size, float r){
+  vec3 q = abs(pos - vec3(0.0,0.0,0.0))- size; 
+  return length(max(q,0.0)) + min((max(q.x,max(q.y,q.z))),0.0) -r;
+}
+
+float SDTorus(vec3 pos, float r1, float r2){
+  vec2 q = vec2(length(pos.xy)-r1,pos.z);
+  vec3 p = 2.0 * vec3(normalize(pos.xy),0.0);
+  return length(q)-r2;
+}
+
 float DistanceToObject(vec3 pos){
-  return(length(pos - vec3(val1/20.0 - 2.5,val2/20.0 - 2.5,-10.0))-3.0);
+  vec3 repeat = fract(pos) - 0.5;
+  return SDTorus(rotate(repeat,vec3(0.5,0.5,0.5),timeSinceStart/10.0),0.3,0.05);
 }
 
 
